6303ee2def56fb0aefadd8ba15dbfde6b2c8a029
Supriya Pabbisetty <supriyasatya.maram@gmail.com>
2021-08-16 21:17:10 +1200

    Mars QA-1 project solution updated from Mentor reviews

---
+++ MarsQA-1/Feature/AddDescriptionSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MarsQA_1.Feature
+{
+    [Binding]
+    public class AddDescriptionSteps
+    {
+        [Given(@"seller able to add Description to my profile")]
+        public void GivenSellerAbleToAddDescriptionToMyProfile()
+        {
+            MarsQA_1.SpecflowPages.Pages.AddProfileDescription.AddDescription();
+        }
+        
+        [Given(@"seller not able to save the Description successfully")]
+        public void GivenSellerNotAbleToSaveTheDescriptionSuccessfully()
+        {
+            MarsQA_1.SpecflowPages.Pages.AddProfileDescription.AddDescriptionnotsuccessfull();
+        }
+    }
+}
---
+++ MarsQA-1/Feature/Description.feature
@@ -0,0 +1,19 @@
+﻿Feature:  Add Description
+	As a Seller
+	I want the feature to add a Description  to my profile
+	so that 
+	People looking for Description can look into my details
+	Acceptance Criteria
+	Seller is able to add the Description
+
+
+@mytag
+Scenario: 01 Test Add Description 
+Given seller able to add Description to my profile
+
+@mytag
+Scenario: 02 Test Add Description unsuccessful
+Given seller not able to save the Description successfully	
+	
+	
+	
